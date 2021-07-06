e4b350c71bd805f0c7ba041b5c7938f1e3b41e6a
aveek12345 <86606545+aveek12345@users.noreply.github.com>
2021-07-06 18:45:37 +0530

    Update runTest.java

--- src/test/java/cucumberJava/runTest.java
+++ src/test/java/cucumberJava/runTest.java
@@ -6,12 +6,10 @@ import cucumber.junit.Cucumber;
 @RunWith(Cucumber.class) 
 @Cucumber.Options(
 
-//format = {"pretty", "html:target/cucumber"}
+format = {"pretty", "html:target/cucumber"}
  
        
-        format= {"pretty","html:test-output_1",
-                "json:target/cucumber-reports/CucumberTestReport.json"},
-        plugin = ("json:target/cucumber-reports/CucumberTestReport.json")
+        
        
  
                  ) 
