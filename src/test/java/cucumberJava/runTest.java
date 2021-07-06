17f1d4692e28ecb88a1cc2323622c323d38bcb44
aveek12345 <86606545+aveek12345@users.noreply.github.com>
2021-07-06 17:51:47 +0530

    Update runTest.java

--- src/test/java/cucumberJava/runTest.java
+++ src/test/java/cucumberJava/runTest.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber; 
 
 @RunWith(Cucumber.class) 
-@Cucumber.Options(format = {"pretty", "html:target/cucumber"}
-                 features="src/test/java/cucumberJava"
+@Cucumber.Options(
+features="src/test/java/cucumberJava",
+format = {"pretty", "html:target/cucumber"}
+ 
                  ) 
 
 public class runTest { }
